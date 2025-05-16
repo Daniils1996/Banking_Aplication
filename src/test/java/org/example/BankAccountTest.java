498642d011d4668616f727fd2ecad2e08a9bce95
Daniils Grigorjevs <daniilsgrigorjevs1@gmails.com>
2025-05-16 10:41:02 +0300

    Added testing for Bank Account Class

---
+++ src/test/java/org/example/BankAccountTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void getAccountHolder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAccountHolder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printBalance() {
+    }
+}
